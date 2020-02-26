802d1f597f735349ef03e210b85eec8273bfe067
Syed Naqvi <syednaqvi@geotab.com>
2020-02-26 12:11:28 -0500

    error display

--- src/utils/stand-alone/login.js
+++ src/utils/stand-alone/login.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
 
-export const LoginPage = props => {
-  return (
-    <form className="login-form" onSubmit={props.handleFormSubmit}>
-      <input id="RTM-login-server" type="text" placeholder="Server name" className="input-row" required ></input>
-      <input id="RTM-login-database" type="text" placeholder="Database name" className="input-row" required ></input>
-      <input id="RTM-login-email" type="email" placeholder="Email" className="input-row" required ></input>
-      <input id="RTM-login-password" type="password" placeholder="Password" className="input-row" required ></input>
-      <input id="login-button" type="submit" value="Login" className="input-row"></input>
-      <div className="login-error input-row" style={{ display: props.showError ? "block" : "none" }}>
-        Invalid user name or password
-      </div>
-    </form>);
-};
+export const LoginPage = props => (
+  <form className="login-form" onSubmit={props.handleFormSubmit}>
+    <input id="RTM-login-server" type="text" placeholder="Server name" className="input-row" required ></input>
+    <input id="RTM-login-database" type="text" placeholder="Database name" className="input-row" required ></input>
+    <input id="RTM-login-email" type="email" placeholder="Email" className="input-row" required ></input>
+    <input id="RTM-login-password" type="password" placeholder="Password" className="input-row" required ></input>
+    <div className="login-error input-row" id="RTM-Login-error">   Invalid user name or password  </div>
+    <input id="login-button" type="submit" value="Login" className="input-row"></input>
+  </form>
+);
